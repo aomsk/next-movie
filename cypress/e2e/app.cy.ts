59e12bbb07c8a8076e10d4e035903c6018b7939d
Sipang K <plaaom39@gmail.com>
2023-07-26 13:04:27 +0700

    test: update app.cy.ts

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,5 +1,11 @@
-describe("template spec", () => {
-  it("Go to next next-movie", () => {
-    cy.visit("http://localhost:3001");
+describe("e2e Test", () => {
+  it("Go to 'The Flash'", async () => {
+    cy.visit("http://localhost:3000");
+    cy.wait(2000);
+    cy.get('a[href*="/movie/298618').click();
+    cy.wait(2000);
+    cy.url().should("include", "/movie/298618");
+    cy.get("h2").contains("The Flash");
+    cy.get('a[href*="/').click();
   });
 });
